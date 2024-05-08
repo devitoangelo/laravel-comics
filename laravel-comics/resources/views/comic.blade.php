ef281547e507f6113c282b8467fbec39afa59a1d
devitoangelo <angelodevito4@gmail.com>
2024-05-08 18:28:38 +0200

    complete layout and

--- laravel-comics/resources/views/comic.blade.php
+++ laravel-comics/resources/views/comic.blade.php
@@ -2,14 +2,47 @@
 
 
 @section('content')
-   
+    <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 g-3">
+            <div class="col">
+                <img src="{{ $comic['thumb'] }}"alt="">
+                <div class="card border-0">
+                    <div class="card-body">
+
+                        <h2 class="card-title">
+                            Titolo : {{ $comic['title'] }}
+                        </h2>
+                        <p class="card-text">
+
+                        <section style="font-size: x-large ">Description :</section> {{ $comic['description'] }}
+                        </p>
+
+                    </div>
+                    <div class="card-footer">
+                        <p>
+                            Prezzo :
+                        <nav style="color: red">
+                            {{ $comic['price'] }} </nav>
+                        </p>
+                        <p>
+                           Type : {{ $comic['type'] }}
+
+                        </p>
+                    </div>
+                </div>
+
+
+
+            </div>
+            <h1>
+
+            </h1>
 
 
 
 
-{{$comic['title']}}
 
+        </div>
 
-
-
+    </div>
 @endsection
